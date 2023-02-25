9e91e64d80e68d6224b0022a2061226eabd73490
andystewart999 <andy.stewart@live.com>
2023-02-26 08:27:45 +1100

    Update const.py

--- const.py
+++ const.py
@@ -1,40 +1,97 @@
-import logging
-import voluptuous as vol
-from datetime import timedelta
-from homeassistant.helpers import config_validation as cv
-from homeassistant.const import (
-    CONF_USERNAME,
-    CONF_IP_ADDRESS,
-    CONF_SCAN_INTERVAL
-)
-LOGGER = logging.getLogger(__package__)
-
+"""Constants for FortiGate Stats."""
 DOMAIN = "fortigate_stats"
-DEFAULT_SCAN_INTERVAL = 10
+DOMAIN_DATA = "{}_data".format(DOMAIN)
 
-CONFIG_SCHEMA_A=vol.Schema(
-            {
-                #vol.Required(CONF_USERNAME): vol.In(["User", "Installer"]),
-                vol.Required(CONF_IP_ADDRESS): str,
-            }
-)
+PLATFORMS = ["sensor"]
+REQUIRED_FILES = [
+    "const.py",
+    "snmp.py",
+    "manifest.json",
+    "sensor.py",
+    "config_flow.py",
+    "translations/en.json",
+]
+VERSION = "0.1.0"
+ISSUE_URL = "https://github.com/andystewart999/fortigate_stats/issues"
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: CONFIG_SCHEMA_A
-    },
-    extra=vol.ALLOW_EXTRA,
-)
+STARTUP = """
+-------------------------------------------------------------------
+{name}
+Version: {version}
+This is a custom component
+If you have any issues with this you need to open an issue here:
+{issueurl}
+-------------------------------------------------------------------
+"""
 
+CONF_NAME = "name"
+#CONF_DS_STATE = "datastore"
+#CONF_HOST_STATE = "vmhost"
+#CONF_LIC_STATE = "license"
+#CONF_VM_STATE = "vm"
 
-def flattenObj(prefix,seperator,obj):
-    result={}
-    for field in obj:
-        val=obj[field]
-        valprefix=prefix+seperator+field
-        if type(val) is dict:
-            sub=flattenObj(valprefix,seperator,val)
-            result.update(sub)
-        else:
-            result[valprefix]=val
-    return result
+DEFAULT_NAME = "FortiGate stats"
+DEFAULT_PORT = 161
+#DEFAULT_DS_STATE = "free_space_gb"
+#DEFAULT_HOST_STATE = "vms"
+#DEFAULT_LIC_STATE = "status"
+#DEFAULT_VM_STATE = "state"
+
+## used to set default states for yaml config.
+#DEFAULT_OPTIONS = {
+#    "datastore": "free_space_gb",
+#    "vmhost": "vms",
+#    "license": "status",
+#    "vm": "state"
+#}
+
+#DATASTORE_STATES = [
+#    "connected_hosts",
+#    "free_space_gb",
+#    "total_space_gb",
+#    "type",
+#    "virtual _machines",
+#]
+
+#LICENSE_STATES = [
+#    "expiration_days",
+#    "status"
+#]
+
+#VMHOST_STATES = [
+#    "cpuusage_ghz",
+#    "memusage_gb",
+#    "state",
+#    "uptime_hours",
+#    "vms"
+#]
+
+#VM_STATES = [
+#    "cpu_use_pct",
+#    "memory_used_mb",
+#    "snapshots",
+#    "status",
+#    "state",
+#    "uptime_hours","
+#    "used_space_gb"
+#]
+
+SNMP_MAP = {
+    "1.3.6.1.4.1.12356.101.4.1.1.0": "FortiOS version",
+    "1.3.6.1.4.1.12356.101.4.1.3.0": "CPU usage (%)",
+    "1.3.6.1.4.1.12356.101.4.1.4.0": "Memory usage (%)",
+    "1.3.6.1.4.1.12356.101.4.1.6.0": "Disk usage (MB)",
+    "1.3.6.1.4.1.12356.101.4.1.7.0": "Disk capacity (MB)", # We need both of these to get a percentage, SNMP won't return it directly
+    "1.3.6.1.4.1.12356.101.4.5.2.0": "Processor module count",
+    "1.3.6.1.4.1.12356.101.4.5.3.1.8": "Sessions", # Append with .1, ,2 etc depending on the processor module count, then sum all the values
+    "1.3.6.1.4.1.12356.101.4.9.1.0": "SD-WAN health check count",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.2": "SD-WAN health check name", # All the health check OIDs need to be appended with .1, .2 etc depending on the health check count
+    "1.3.6.1.4.1.12356.101.4.9.2.1.4": "SD-WAN health check state",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.5": "SD-WAN health check latency",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.9": "SD-WAN health check packet loss",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.11": "SD-WAN health check inbound bandwidth (estimated)",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.12": "SD-WAN health check outbound bandwidth (estimated)",
+    "1.3.6.1.4.1.12356.101.4.9.2.1.14": "SD-WAN health check interface name".
+}
+
+SUPPORTED_PRODUCTS = ["FortiGate", "FortiGate"]
